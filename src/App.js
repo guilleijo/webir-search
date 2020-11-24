1f23ace2343f910f64b64e177e351f65e40c03ef
Guillermo Eijo <guillermoeijo@gmail.com>
2020-11-24 04:06:22 -0300

    V2

--- src/App.js
+++ src/App.js
@@ -9,32 +9,9 @@ import {
   MultiDropdownList,
   RangeInput,
   MultiList,
-  ToggleButton,
 } from '@appbaseio/reactivesearch';
 
 const { ResultCardsWrapper } = ReactiveList;
-
-// brand
-// model
-// doors
-// km
-// fuel type
-// price
-// currency
-// condition
-// publication link
-// publication Title
-// thumbnail 
-
-// seller
-//  - reputation 
-//    - transactions: total, canceled, completed, ratings 
-// available quantity
-// address
-//   - state name (location eg montevideo)
-//   - city name (sayago)
-// sigle owner
-// control de traccion
 
 
 const openInNewTab = (url) => {
@@ -48,122 +25,226 @@ class App extends Component {
       <ReactiveBase
         url='http://localhost:9200/'
         app='autos'>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div style={{ display: 'flex', flexDirection: 'column', width: '30%' }}>
 
-        <DataSearch
-          componentId='SearchFilter'
-          dataField={['model', 'brand']}
-          fuzziness={2}
-          placeholder='Buscar por marca o modelo'
-        />
+            <DataSearch
+              componentId='SearchFilter'
+              dataField={['model', 'brand']}
+              fuzziness={2}
+              placeholder='Buscar por marca o modelo'
+              style={{
+                padding: "5px",
+                marginTop: "10px"
+              }}
+            />
 
-        <div className='col'>
-          <MultiDropdownList
-            componentId='BrandSensor'
-            dataField='brand.keyword'
-            placeholder='Seleccionar marca'
+            <div className='col'>
+              <MultiDropdownList
+                componentId='BrandSensor'
+                dataField='brand.keyword'
+                placeholder='Seleccionar marca'
+                style={{
+                  padding: "5px",
+                  marginTop: "10px"
+                }}
+              />
+            </div>
+
+            <div className='col'>
+              <MultiList
+                showSearch={false}
+                componentId="CurrencySensor"
+                dataField="currency.keyword"
+                title="Moneda"
+                react={{
+                  and: [
+                    'SearchFilter',
+                    'BrandSensor',
+                    'RangeInputSensor',
+                    'ConditionSensor',
+                    'FuelSensor',
+                    'DoorsSensor',
+                  ],
+                }}
+                style={{
+                  padding: "5px",
+                  marginTop: "10px"
+                }}
+              />
+            </div>
+
+            <RangeInput
+              componentId='RangeInputSensor'
+              dataField='price'
+              title='Precio'
+              range={{
+                start: 3000,
+                end: 50000,
+              }}
+              defaultValue={{
+                start: 0,
+                end: 50000,
+              }}
+              rangeLabels={{
+                start: '$0',
+                end: '$50000',
+              }}
+              showFilter={true}
+              stepValue={1}
+              showHistogram={true}
+              interval={2}
+              react={{
+                and: [
+                  'SearchFilter',
+                  'BrandSensor',
+                  'CurrencySensor',
+                  'ConditionSensor',
+                  'FuelSensor',
+                  'DoorsSensor',
+                ],
+              }}
+              URLParams={false}
+              includeNullValues
+              style={{
+                padding: "5px",
+                marginTop: "10px"
+              }}
+            />
+
+            <div className='col'>
+              <MultiList
+                showSearch={false}
+                componentId="ConditionSensor"
+                dataField="condition.keyword"
+                title="Condición"
+                react={{
+                  and: [
+                    'SearchFilter',
+                    'BrandSensor',
+                    'CurrencySensor',
+                    'RangeInputSensor',
+                    'FuelSensor',
+                    'DoorsSensor',
+                  ],
+                }}
+                style={{
+                  padding: "5px",
+                  marginTop: "10px"
+                }}
+              />
+            </div>
+
+            <div className='col'>
+              <MultiList
+                showSearch={false}
+                componentId="FuelSensor"
+                dataField="fuel_type.keyword"
+                title="Tipo de combustible"
+                react={{
+                  and: [
+                    'SearchFilter',
+                    'BrandSensor',
+                    'CurrencySensor',
+                    'RangeInputSensor',
+                    'ConditionSensor',
+                    'DoorsSensor',
+                  ],
+                }}
+                style={{
+                  padding: "5px",
+                  marginTop: "10px"
+                }}
+              />
+            </div>
+
+            <div className='col'>
+              <MultiList
+                showSearch={false}
+                componentId="DoorsSensor"
+                dataField="doors.keyword"
+                title="Puertas"
+                react={{
+                  and: [
+                    'SearchFilter',
+                    'BrandSensor',
+                    'CurrencySensor',
+                    'RangeInputSensor',
+                    'ConditionSensor',
+                    'FuelSensor',
+                  ],
+                }}
+                style={{
+                  padding: "5px",
+                  marginTop: "10px"
+                }}
+              />
+            </div>
+
+            <SelectedFilters
+              showClearAll={true}
+              clearAllLabel='Clear filters'
+              style={{
+                padding: "5px",
+                marginTop: "10px"
+              }}
+            />
+
+          </div>
+
+          <ReactiveList
+            componentId='results'
+            dataField='name'
+            size={8}
+            pagination={true}
+            react={{
+              and: [
+                'SearchFilter',
+                'BrandSensor',
+                'CurrencySensor',
+                'RangeInputSensor',
+                'ConditionSensor',
+                'FuelSensor',
+                'DoorsSensor',
+              ]
+            }}
+            style={{
+              width: "70%",
+              textAlign: "center"
+            }}
+
+
+            render={({ data }) => (
+              <ResultCardsWrapper>
+                {
+                  data.map(item => (
+                    <ResultCard
+                      key={item._id}
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => { openInNewTab(item.publication_link) }}
+                    >
+                      <ResultCard.Image
+                        src={item.thumbnail}
+                      />
+                      <ResultCard.Title
+                        dangerouslySetInnerHTML={{
+                          __html: item.brand + ' - ' + item.model
+                        }}
+                      />
+                      <ResultCard.Description>
+                        {item.currency + ' ' + item.price}
+                      </ResultCard.Description>
+                      <ResultCard.Description>
+                        {item.kilometers}
+                      </ResultCard.Description>
+                    </ResultCard>
+                  ))
+                }
+              </ResultCardsWrapper>
+            )}
+
           />
         </div>
-
-        <div className='col'>
-          <MultiList
-            showSearch={false}
-            componentId="CurrencySensor"
-            dataField="currency.keyword"
-            title="Moneda"
-          />
-        </div>
-
-        <div className='col'>
-          <MultiList
-            showSearch={false}
-            componentId="ConditionSensor"
-            dataField="condition.keyword"
-            title="Condición"
-          />
-        </div>
-
-        <RangeInput
-          componentId='RangeInputSensor'
-          dataField='price'
-          title='Precio'
-          range={{
-            start: 3000,
-            end: 50000,
-          }}
-          defaultValue={{
-            start: 3000,
-            end: 50000,
-          }}
-          rangeLabels={{
-            start: 'Start',
-            end: 'End',
-          }}
-          showFilter={true}
-          stepValue={1}
-          showHistogram={true}
-          interval={2}
-          react={{
-            and: [
-              'SearchFilter',
-              'BrandSensor',
-              'CurrencySensor',
-              'ConditionSensor',
-            ],
-          }}
-          URLParams={false}
-          includeNullValues
-        />
-
-        <SelectedFilters
-          showClearAll={true}
-          clearAllLabel='Clear filters'
-        />
-
-        <ReactiveList
-          componentId='results'
-          dataField='name'
-          size={8}
-          pagination={true}
-          react={{
-            and: [
-              'SearchFilter',
-              'BrandSensor',
-              'CurrencySensor',
-              'RangeInputSensor',
-              'ConditionSensor',
-              'TagCloud01'
-            ]
-          }}
-
-
-          render={({ data }) => (
-            <ResultCardsWrapper>
-              {
-                data.map(item => (
-                  <ResultCard key={item._id}
-                    onClick={() => {openInNewTab(item.publication_link)}}
-                  >
-                    <ResultCard.Image
-                      src={item.thumbnail}
-                    />
-                    <ResultCard.Title
-                      dangerouslySetInnerHTML={{
-                        __html: item.model
-                      }}
-                    />
-                    <ResultCard.Description>
-                      {item.brand}
-                    </ResultCard.Description>
-                    <ResultCard.Description>
-                      {item.currency + ' ' + item.price}
-                    </ResultCard.Description>
-                  </ResultCard>
-                ))
-              }
-            </ResultCardsWrapper>
-          )}
-
-        />
 
       </ReactiveBase>
     );
